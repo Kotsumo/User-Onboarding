51be11aec881b2d595fafbe570e2235ea833f664
David <davidellis1020@gmail.com>
2021-02-25 20:10:31 -0800

    Created form, used yup validation, used post and stored data

---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+export default function Form(props) {
+    const {
+        values, 
+        change, 
+        submit,
+        disabled
+    } = props
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    const onChange = evt => {
+        const {name, value, type, checked} = evt.target
+        const ValueToUse = type === 'checkbox' ? checked : value
+        change(name, ValueToUse)
+    }
+
+    return (
+        <form className='form container' onSubmit={onSubmit}>
+            <div className='form-group submit'>
+                <h2>Add a user</h2>
+
+                <button disabled={disabled}>submit</button>
+
+
+                <label>Name
+                    <input
+                        name='name'
+                        type='text'
+                        onChange={onChange}
+                        value={values.name}
+                        placeholder='type a name...'
+                        maxLength=''
+                    />
+                </label>
+
+                <label>Email
+                    <input
+                        name='email'
+                        type='email'
+                        onChange={onChange}
+                        value={values.email}
+                        placeholder='type an email...'
+                        maxLength='30'
+                    />
+                </label>
+
+                <label>Password
+                    <input
+                        name='password'
+                        type='password'
+                        onChange={onChange}
+                        value={values.password}
+                        placeholder='type a password...'
+                        maxLength='30'
+                    />
+                </label>
+
+                <label>TOS
+                    <input
+                        type='checkbox'
+                        name='tos'
+                        onChange={onChange}
+                        checked={values.tos}
+                    />
+                </label>
+            </div>
+        </form>
+    )
+}
