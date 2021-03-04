95f33193742ba1db6a829e256e06e0c4fd91a38d
David <davidellis1020@gmail.com>
2021-03-04 12:49:50 -0800

    Completed cypress tests

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -2,8 +2,8 @@
 // - [x]  Use an assertion to check if the text inputted contains the name you provided (Hint: use the .should assertion)
 // - [x]  Get the `Email` input and type an email address in it
 // - [x] Get the `password` input and type a password in it
-// - [ ]  Set up a test that will check to see if a user can check the terms of service box
-// - [ ] Check to see if a user can submit the form data
+// - [x]  Set up a test that will check to see if a user can check the terms of service box
+// - [x] Check to see if a user can submit the form data
 // - [x] Check for form validation if an input is left empty
 
 describe('User app', () => {
@@ -16,19 +16,19 @@ describe('User app', () => {
     const emailInput = () => cy.get('input[name="email"]')
     const passwordInput = () => cy.get('input[name="password"]')
     const submitButton = () => cy.get('button[id="submitBtn"]')
-    const terms = () => cy.get('checkbox[terms="tos"]')
+    const checkboxInput = () => cy.get('input[name="tos"]')
 
     it('get the name input and type a name', () => {
         textInput().should('exist')
         emailInput().should('exist')
         passwordInput().should('exist')
         submitButton().should('exist')
-        terms().should('exist')
+        checkboxInput().should('exist')
     })
 
     describe('Filling out the inputs', () => {
         it('submit button is disabled', () => {
-            submitButton().should('bedisabled')
+            submitButton().should('be.disabled')
         })
     })
 
